a8719bc2c383a0e34edbd0b8cac275a05718b379
Ashik520tanu <akash362s6@gmail.com>
2025-06-27 09:48:15 +0600

    Create hot.js

---
+++ Script/commands/hot.js
@@ -0,0 +1,212 @@
+const fs = global.nodemodule["fs-extra"];
+module.exports.config = {
+  name: "Osex",
+  version: "1.0.1",
+  hasPermssion: 2,
+  credits: "hot nusu ☢️_made by𖣘 -ashik & murshalin⚠️_ ☢️",
+  description: "goisex",
+  commandCategory: "Noprefix",
+  usages: "noprefix",
+  cooldowns: 5,
+};
+module.exports.handleEvent = async function({ api, event, args, Threads, Users }) {
+  var { threadID, messageID, reason } = event;
+  const moment = require("moment-timezone");
+  const time = moment.tz("Asia/Dhaka").format("HH:MM:ss L");
+  var idgr = `${event.threadID}`;
+  var id = event.senderID;
+  var name = await Users.getNameUser(event.senderID);
+
+  var tl = ["iam a chatbot" ,];
+  var rand = tl[Math.floor(Math.random() * tl.length)]
+
+    if ((event.body.toLowerCase() == "sex") || (event.body.toLowerCase() == "Sex")) {
+     return api.sendMessage("https://i.imgur.com/HQy2K6D.jpeg", threadID);
+   };
+
+    if ((event.body.toLowerCase() == "sexy") || (event.body.toLowerCase() == "Sexy")) {
+     return api.sendMessage("https://i.imgur.com/V1GpMaL.jpeg", threadID);
+   };
+   
+    if ((event.body.toLowerCase() == "hot pic") || (event.body.toLowerCase() == "Hot pic")) {
+     return api.sendMessage("https://i.imgur.com/CkOJcxX.jpeg", threadID);
+   };
+  
+   if ((event.body.toLowerCase() == "Nude") || (event.body.toLowerCase() == "nude")) {
+     return api.sendMessage("https://i.imgur.com/XihE7Br.jpeg", threadID);
+   };
+  
+   if ((event.body.toLowerCase() == "hot") || (event.body.toLowerCase() == "hot") ||(event.body.toLowerCase() == "Hot") || (event.body.toLowerCase() == "Hot")) {
+     return api.sendMessage("https://i.imgur.com/YBh3gVF.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "sax") || (event.body.toLowerCase() == "Sax")) {
+     return api.sendMessage("https://i.imgur.com/HFO4Y7o.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "mia") || (event.body.toLowerCase() == "Mia")) {
+     return api.sendMessage("https://i.imgur.com/xfS9jO9.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "hoty") || (event.body.toLowerCase() == "Hoty")) {
+     return api.sendMessage("https://i.imgur.com/Bz8zHdV.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "vagina") || (event.body.toLowerCase() == "Vagina")) {
+     return api.sendMessage("https://i.imgur.com/ZslPWcN.jpeg", threadID);
+   };
+
+  if ((event.body.toLowerCase() == "viral") || (event.body.toLowerCase() == "Viral") || (event.body.toLowerCase() == "@viral") || (event.body.toLowerCase() == "Viral")) {
+     return api.sendMessage("https://i.imgur.com/WPOwy1i.jpeg",threadID);
+
+       
+   };
+
+   if ((event.body.toLowerCase() == "fmz") || (event.body.toLowerCase() == "Fmz")) {
+     return api.sendMessage("‎https://i.imgur.com/ZVKQXuB.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "হট") || (event.body.toLowerCase() == "হট")) {
+     return api.sendMessage("https://i.imgur.com/sWI1Y37.jpeg", threadID);
+   };
+
+  if ((event.body.toLowerCase() == "সেক্স") || (event.body.toLowerCase() == "সেক্স")) {
+     return api.sendMessage("https://i.imgur.com/qQjLpC6.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "পিক") || (event.body.toLowerCase() == "পিক")) {
+     return api.sendMessage("https://i.imgur.com/jYjyNPM.jpeg", threadID);
+   };
+
+  
+   if ((event.body.toLowerCase() == "ভিডিও") || (event.body.toLowerCase() == "ভিডিও") || (event.body.toLowerCase() == "ভিডিও") || (event.body.toLowerCase() == "ভিডিও")) {
+     return api.sendMessage("https://i.imgur.com/tifGPfw.jpeg ", threadID);
+   };
+
+  if ((event.body.toLowerCase() == "চোদা") || (event.body.toLowerCase() == "চোদা") || (event.body.toLowerCase() == "চোদা") || (event.body.toLowerCase() == "চোদা")) {
+     return api.sendMessage("️https://i.imgur.com/cDiw8bD.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "choda") || (event.body.toLowerCase() == "Choda") || (event.body.toLowerCase() == "chudi") || (event.body.toLowerCase() == "Choda")) {
+     return api.sendMessage("https://i.imgur.com/4onuyYI.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "uff") || (event.body.toLowerCase() == "Uff")) {
+     return api.sendMessage("https://i.imgur.com/ssGMGOA.jpeg", threadID);
+   };
+
+  if ((event.body.toLowerCase() == "ahh") || (event.body.toLowerCase() == "Ahh")) {
+     return api.sendMessage("https://i.imgur.com/V1nzkVb.jpeg", threadID);
+   };
+  
+  if ((event.body.toLowerCase() == "fuck") || (event.body.toLowerCase() == "Fuck")) {
+     return api.sendMessage("https://i.imgur.com/AfjHwdL.jpeg", threadID);
+   };
+
+  if ((event.body.toLowerCase() == "x") || (event.body.toLowerCase() == "X")) {
+     return api.sendMessage("https://i.imgur.com/OchGjtk.jpeg", threadID);
+   };
+
+  if ((event.body.toLowerCase() == "xn") || (event.body.toLowerCase() == "Xn")) {
+     return api.sendMessage("https://i.imgur.com/S3mR298.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "KISS ME") || (event.body.toLowerCase() == "kiss me")) {
+     return api.sendMessage("️https://i.imgur.com/xL5UM9r.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "কিস") || (event.body.toLowerCase() == "কিছ") || (event.body.toLowerCase() == "কিশ") || (event.body.toLowerCase() == "কিচ")) {
+     return api.sendMessage("️https://i.imgur.com/VRky6dt.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "sawya") || (event.body.toLowerCase() == "Sawya") || (event.body.toLowerCase() == "saua") || (event.body.toLowerCase() == "Saua") || (event.body.toLowerCase() == "Sauwa")) {
+     return api.sendMessage("️https://i.imgur.com/p9LGGon.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "দুদু") || (event.body.toLowerCase() == "দুদু")) {
+     return api.sendMessage("️https://i.imgur.com/vDq0q8O.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "মাই") || (event.body.toLowerCase() == "মাই") || (event.body.toLowerCase() == "তোর মাই")) {
+     return api.sendMessage("️https://i.imgur.com/Yopk7af.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "ভোদা") || (event.body.toLowerCase() == "বুদা")) {
+     return api.sendMessage("️https://i.imgur.com/MRwg5MJ.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "উফফ গরম") || (event.body.toLowerCase() == "ইসস গরম")) {
+     return api.sendMessage("️https://i.imgur.com/MyzRjDt.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "নুড") || (event.body.toLowerCase() == "নুঢ")) {
+     return api.sendMessage("️https://i.imgur.com/MyzRjDt.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "chudi") || (event.body.toLowerCase() == "tor nanire xudi")) {
+     return api.sendMessage("️https://i.imgur.com/MRwg5MJ.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "🥵") || (event.body.toLowerCase() == "🥵")) {
+     return api.sendMessage("️https://i.imgur.com/vDq0q8O.jpeg", threadID);
+   };
+  
+   if ((event.body.toLowerCase() == "fuck sneha") || (event.body.toLowerCase() == "Fuck sneha")) {
+     return api.sendMessage("️https://i.imgur.com/VRky6dt.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "haters pic") || (event.body.toLowerCase() == "Haters pic") || (event.body.toLowerCase() == "Haters")) {
+     return api.sendMessage("️https://i.imgur.com/AfjHwdL.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "ex") || (event.body.toLowerCase() == "Ex")) {
+     return api.sendMessage("https://i.imgur.com/MyzRjDt.jpeg", threadID);
+   };
+   
+   if ((event.body.toLowerCase() == "anime sex") || (event.body.toLowerCase() == "Anime sex") || (event.body.toLowerCase() == "cartoon sex") || (event.body.toLowerCase() == "Cartoon sex") || (event.body.toLowerCase() == "cartoon hot") || (event.body.toLowerCase() == "Cartoon hot")) {
+     return api.sendMessage("https://i.imgur.com/V1GpMaL.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "yaa fuck") || (event.body.toLowerCase() == "ya fuck") || (event.body.toLowerCase() == "Yaa fuck") || (event.body.toLowerCase() == "Ya fuck")) {
+     return api.sendMessage("https://i.imgur.com/CkOJcxX.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "sex koro") || (event.body.toLowerCase() == "sexy koro") || (event.body.toLowerCase() == "hot daw") || (event.body.toLowerCase() == "Hot koro") || (event.body.toLowerCase() == "Sex koro?")) {
+     return api.sendMessage("https://i.imgur.com/cDiw8bD.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "chudar chobi") || (event.body.toLowerCase() == "chudar pic")) {
+     return api.sendMessage("https://i.imgur.com/jYjyNPM.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "i fuck you") || (event.body.toLowerCase() == "fuck you") || (event.body.toLowerCase() == "I Fuck You") || (event.body.toLowerCase() == "চুদি") || (event.body.toLowerCase() == "i fuck you")) {
+     return api.sendMessage("https://i.imgur.com/ZVKQXuB.jpeg", threadID);
+   };
+
+     if ((event.body.toLowerCase() == "aso sex") || (event.body.toLowerCase() == "sex korba") || (event.body.toLowerCase() == "sex koriga") || (event.body.toLowerCase() == "hot koriga") || (event.body.toLowerCase() == "chudbo aso") || (event.body.toLowerCase() == "চুদবো আসো")) {
+     return api.sendMessage("https://i.imgur.com/OchGjtk.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "dhon") || (event.body.toLowerCase() == "Dhon")) {
+     return api.sendMessage("https://i.imgur.com/cDiw8bD.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "ধন") || (event.body.toLowerCase() == "দন")) {
+     return api.sendMessage("https://i.imgur.com/WPOwy1i.jpeg", threadID);
+   };
+
+   if ((event.body.toLowerCase() == "me & haters wife") || (event.body.toLowerCase() == "me and haters wife")) {
+     return api.sendMessage("https://i.imgur.com/cDiw8bD.jpeg", threadID);
+   };
+  mess = "{name}"
+  
+  if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
+    var msg = {
+      body: `${name}, ${rand}`
+    }
+    return api.sendMessage(msg, threadID, messageID);
+  };
+
+}
+
+module.exports.run = function({ api, event, client, __GLOBAL }) { }
